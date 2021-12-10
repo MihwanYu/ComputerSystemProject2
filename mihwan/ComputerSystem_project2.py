737db399bc82bdd311bfbe8dc109bc4c7bfbc500
MihwanYu <sally9804@gmail.com>
2021-12-10 14:11:43 +0900

    주석 추가

--- mihwan/ComputerSystem_project2.py
+++ mihwan/ComputerSystem_project2.py
@@ -6,10 +6,13 @@ Created on Wed Dec  8 15:07:52 2021
 """
 import re
 import time
+from os import chdir, getcwd
+wd=getcwd()
+chdir(wd)
 #lyrics: on the ground + thank you next + rain on me
 
 #%% data load and preprocessing
-with open('twenties_mine.txt', 'r') as f:
+with open('./../Mariah Carey_1990s.txt', 'r') as f:
     lyrics= f.read()
 #전처리: 특수문자 제거, 알파벳 소문자 변환, 어절 분리
 new_lyric = lyrics.replace('\n',' ')
@@ -17,7 +20,7 @@ new_lyric = re.sub(r"[^a-zA-Z0-9 ]","",new_lyric).lower().split()
 print(new_lyric)
 
 #i, me 같이 무의미하게 반복되는 친구들(stopwords)
-with open('stopwords_english.txt', 'r') as f:
+with open('./../stopwords_english.txt', 'r') as f:
     stopwords= f.read().split()
 
 #stopwords 제거
@@ -37,21 +40,25 @@ for word in new_lyric:
 print(word_count_iteration)
 print('end time: ',time.time()-s_time)
 
-
+#캐시블록: 캐시 레벨 안에 여러개 들어갈 수 있음. 1개 블록에는 valid, tag, memory block(offset)을 가짐
 class cacheblock:
     def __init__(self):
         self.valid = False
         self.tag = 0
         self.memoryblock={} #offsets: 0-7
     
+    #isfull(): 8개 메모리 블록이 모두 다 찼으면 true, 아니면 false반환
     def isfull(self):
         if len(self.memoryblock)==8: return True
         else: return False
     
+    #메모리 블록에 단어 쌍을 추가함. word(key) : 1(frequency)
     def insert_into_offset(self, word):
         self.memoryblock[word] = 1
         
-        
+
+#캐시레벨: cache hierarchy를 위해 만듦.
+#캐시 레벨 객체를 만들 때는 안에 몇 개 set있는지, block있는지, 하위 레벨 cache는 누군지 설정해줘야함
 class cachelevel:
     def __init__(self, sets, low_level=None, E=1):
         self.sets = sets
@@ -59,14 +66,16 @@ class cachelevel:
         self.E = E
         self.low_level = low_level
     
+    #캐시블록 배열 생성(레벨 객체 __init__에서 생성할 때 사용)
     def make_blocks(self):#일단 E=1일때로
         temp_blocks=[]
         for i in range(self.sets):
             temp_blocks.append(cacheblock())
         return temp_blocks
     
-    def find_word(self, word): #현재 레벨에 해당 단어 있는지 체크. 없으면 None 리턴,
-                                #있으면 해당 cacheblock 및 단어의 메모리 블록 인덱스 리턴
+    #해당 레벨에 단어가 있는지 찾고, 찾으면 단어가 존재하는 블록 객체를 리턴함
+    #단어가 없으면 하위 레벨에서 단어를 찾고, 단어가 존재하는 블록 객체를 리턴함
+    def find_word(self, word):
         for b in self.blocks:
             if word in b.memoryblock:
                 print('find word: located in block ',b)
@@ -74,6 +83,7 @@ class cachelevel:
             
         return self.find_word_low_level(word)#근데 사실 찾았으면 빈도수 +1해줘야함
     
+    #find_word()에서 사용하기 위한 함수. 하위 level 에서 단어 찾아오고, 상위 레벨에 단어가 존재하는 블록을 로드해줌
     def find_word_low_level(self, word):
         if self.low_level is not None:
             return self.low_level.find_word(word)
@@ -81,6 +91,7 @@ class cachelevel:
             self.insert_data(word)
             return self.find_word(word)
         
+    #단어가 블록에 존재하지 않을 때 블록에 넣어줌
     def insert_data(self, word):
         for b in self.blocks:
             if b.isfull() is False:
